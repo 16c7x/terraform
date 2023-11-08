07c668b24d747db97fc7b21a2dbc2e6bfdeb9c2d
Andrew Jones <andrew.jones@puppet.com>
2023-11-08 16:10:31 +0000

    fix for issue #1

---
+++ pe_env/windows_nodes.tf
@@ -0,0 +1,16 @@
+## Instance
+
+resource "aws_instance" "windows_node" {
+  ami                    = "${var.windows_ami}"
+  instance_type          = "${var.aws_ami_size}"
+  count                  = 0
+  key_name               = "${var.key}"
+  subnet_id              = module.networking.subnet_ids[count.index % length(module.networking.subnet_ids)]
+  vpc_security_group_ids = module.networking.security_group_ids
+
+  tags = {
+    Name     = "windows_node_${count.index}"
+    lifetime = "${var.lifetime}"
+    email    = "andrew.jones@perforce.com"
+  }
+}
